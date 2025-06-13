dec5aaf193a69ce905d74142285c7d12cd4babe2
amckay123 <abbym436@deltalearns.ca>
2025-06-13 14:26:51 -0700

    story and words input
    Wrote the mad lib and the word input.

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 # Welcome + name
-
+print("Hello! Welcome to Abby's awesome mad lib, ABB LIB!!!")
+playername = input("What is your name? Please type here and press enter when you're done. ")
+print(f"Welcome to ABB LIB, {playername}!")
+score = 0
 # Enter words
+print("To start, please write out some words.")
+name = input("Please type a NAME here and press enter when you're done. ")
+number = input("Please type a NUMBER here and press enter when you're done. ")
+object1 = input("Please type an OBJECT here and press enter when you're done. ")
+food = input("Please type a FOOD here and press enter when you're done. ")
+adjective = input("Please type an ADJECTIVE here and press enter when you're done. ")
+verb = input("Please type a VERB here and press enter when you're done. ")
+verb2 = input("Please type a VERB here and press enter when you're done. ")
+place = input("Please type a PLACE here and press enter when you're done. ")
+emotion = input("Please type an EMOTION here and press enter when you're done. ")
+action = input("Please type an ACTION here and press enter when you're done. ")
+action2 = input("Please type an ACTION here and press enter when you're done. ")
+adjective2 = input("Please type an ADJECTIVE here and press enter when you're done. ")
 
 # Write mad lib
 
+print(f"\nI decided to go on a vacation to Australia with {name}. We got to the airport {number} hours early. When we went through security, I got stopped because I forgot to take {object1} out of my pocket. We got some {food} for the flight and arrived at the gate. Once we boarded the plane, I was sitting next to a very {adjective} man. He spent the entire flight {verb} and talking about his job doing {verb2}. Whenever I tried to sleep, he would step around me to go to the {place}. I was so {emotion}. Since I couldn't sleep, I decided to {action} and {action2} instead. Finally, we arrived in Australia. All in all, the flight was {adjective}!")
+
 # Thanks for playing, name!
+print(f"\nThanks for playing, {playername}")
