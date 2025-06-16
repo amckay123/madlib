015c21edac91463292d72d2c34a3205a9d67d0fe
amckay123 <abbym436@deltalearns.ca>
2025-06-16 09:16:37 -0700

    story edits
    made the story better

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ print(f"Welcome to ABB LIB, {playername}!")
 # Enter words
 print("To start, please write out some words.")
 name = input("Please type a NAME here and press enter when you're done. ")
-city = input("Please type a CITY here and press enter when you're done. ")
+city = input("Please type a PLACE here and press enter when you're done. ")
 number = input("Please type a NUMBER here and press enter when you're done. ")
 object1 = input("Please type an OBJECT here and press enter when you're done. ")
 food = input("Please type a FOOD here and press enter when you're done. ")
@@ -21,7 +21,7 @@ adjective2 = input("Please type an ADJECTIVE here and press enter when you're do
 
 # Write mad lib
 
-print(f"\nI decided to go on a vacation to Australia with my #1 bestest friend, {name}. We got to the {city} airport {number} hours early. When we went through security, I got stopped because I forgot to take {object1} out of my pocket. We got some {food} for the flight and arrived at the gate. Once we boarded the plane, I was sitting next to a very {adjective} man. He spent the entire flight {verb} and talking about his job doing {verb2}. Whenever I tried to sleep, he would step around me to go to the {place}. I was so {emotion}. Since I couldn't sleep, I decided to go {action} and {action2} instead. Finally, we arrived in Australia. All in all, the flight was {adjective2}!")
+print(f"\nI decided to go on a vacation to {city} with my #1 bestest friend, {name}. We got to the airport {number} hours early. When we went through security, I got stopped because I forgot to take granny's {object1} out of my pocket. We got some delicious {food} for the flight and finally arrived at the gate. Once we boarded the plane, I was unfortunately sitting next to a very {adjective} man. He spent the entire flight {verb} and talking about his weird job doing {verb2}. Whenever I tried to sleep, he would step around me to go to the {place}. I was so {emotion}. Since I couldn't sleep, I decided to go {action} and {action2} instead. Finally, we arrived in {city}. All in all, the flight was {adjective2}!")
 
 # Thanks for playing, name!
 print(f"\nThanks for playing, {playername}!")
